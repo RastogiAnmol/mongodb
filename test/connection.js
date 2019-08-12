9f7247127ea10d799887c03ac44fecd7d24c38ce
Anmol Rastogi <anmolamity@gmail.com>
2019-08-12 22:12:55 +0530

    created connection and wrote test to store data in DB and test it

--- test/connection.js
+++ test/connection.js
@@ -1,13 +1,8 @@
 
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const uri = "mongodb+srv://anmol:anmolrastogi@cluster0-m2epa.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-client.once('open', function(){
+mongoose.connect(uri, {useNewUrlParser: true});
+mongoose.connection.once('open', function(){
     console.log('Connection has been made, now make fireworks...');
 }).on('error', function(error){
     console.log('Connection error:', error);
---
+++ test/demo_test.js
@@ -0,0 +1,37 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+describe('some demo test', function () {
+
+    // create tests
+    it('adds two numbers', function () {
+        assert(2 + 3 == 5)
+    })
+    // done parameter is used to tell mocha that our test is complete and it can continue to new test present below
+    it('saves a record to the DB', function (done) {
+        var record = new MarioChar({
+            name: 'Mario',
+            weight: 40
+        });
+        record.save().then(()=>{
+            /* record.isNew === false when data is saved to DB
+            ** record.isNew === true , it remains true until data is saved to DB
+            */
+            assert(record.isNew === false);
+            done();
+        });
+    })
+    it('saves another record to the DB', function (done) {
+        var record = new MarioChar({
+            name: 'Luigi',
+            weight: 40
+        });
+        record.save().then(()=>{
+            /* record.isNew === false when data is saved to DB
+            ** record.isNew === true , it remains true until data is saved to DB
+            */
+            assert(record.isNew === false);
+            done();
+        });
+    })
+})
